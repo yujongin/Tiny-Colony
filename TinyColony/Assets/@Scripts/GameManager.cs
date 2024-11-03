f522ac3d3645cd547c28f6ee3a85228a334e7d58
yujongin <48309991+yujongin@users.noreply.github.com>
2024-11-03 23:20:42 +0900

    Day1

--- TinyColony/Assets/@Scripts/GameManager.cs
+++ TinyColony/Assets/@Scripts/GameManager.cs
@@ -1,16 +1,70 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
-    void Start()
+  public List<Gate> gates;
+  public ItemSpawner itemSpawner;
+  int goalPoint;
+  int currentPoint;
+  private static GameManager instance;
+
+  public static GameManager Instance
+  {
+    get
     {
-        
+      if (instance == null)
+      {
+        return null;
+      }
+      return instance;
+    }
+  }
+  void Awake()
+  {
+    if (instance == null)
+    {
+      instance = this;
+      DontDestroyOnLoad(gameObject);
+    }
+    else
+    {
+      Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    itemSpawner = GameObject.Find("ItemSpawner").GetComponent<ItemSpawner>();
+  }
+  void Start()
+  {
+    for (int i = 0; i < gates.Count; i++)
     {
-        
+      goalPoint += gates[i].maxHuman;
     }
+
+  }
+
+  public void UpdatePoint()
+  {
+    currentPoint++;
+    if (goalPoint == currentPoint)
+    {
+      StageClear();
+    }
+  }
+
+  public void StageClear()
+  {
+    Debug.Log("stage Clear!");
+  }
+
+  public void GameOver()
+  {
+    Debug.Log("GameOver");
+  }
+
+  public void Restart()
+  {
+    //scene reload
+  }
 }
--- TinyColony/Assets/@Scripts/Launcher.cs
+++ TinyColony/Assets/@Scripts/Launcher.cs
@@ -4,95 +4,91 @@ using UnityEngine.EventSystems;
 
 public class Launcher : MonoBehaviour
 {
-    public GameObject bucket;
-    public List<Transform> linePoints;
-    public float speed = 3;
-    public List<GameObject> items;
+  public GameObject bucket;
+  public List<Transform> linePoints;
+  public float speed = 3;
+  public List<GameObject> items;
 
-    LineRenderer handleLineRenderer;
-    Vector3 shootDir;
-    
-    bool isShoot = false;
-    void Start()
+  LineRenderer handleLineRenderer;
+  Vector3 shootDir;
+
+  bool isShoot = false;
+  void Start()
+  {
+    handleLineRenderer = GetComponent<LineRenderer>();
+    handleLineRenderer.positionCount = 3;
+    handleLineRenderer.startWidth = 0.01f;
+    handleLineRenderer.endWidth = 0.01f;
+
+
+    //test
+    AddItem(GameObject.Find("Human"));
+  }
+
+  Vector2 startPoint;
+  void Update()
+  {
+    if (!EventSystem.current.IsPointerOverGameObject())
     {
-        handleLineRenderer = GetComponent<LineRenderer>();
-        handleLineRenderer.positionCount = 3;
-        handleLineRenderer.startWidth = 0.01f;
-        handleLineRenderer.endWidth = 0.01f;
+      if (Input.GetMouseButtonDown(0))
+      {
+        startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+      }
+
+      if (Input.GetMouseButton(0))
+      {
+        Vector2 currentPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 dir = startPoint - currentPoint;
+        dir = Vector2.ClampMagnitude(dir, 3f);
+
+        bucket.transform.position = transform.position - new Vector3(dir.x, dir.y, 0);
 
 
-        //test
-        AddItem(GameObject.Find("Human"));
+        shootDir = transform.position - bucket.transform.position;
+        DrawLine();
+
+      }
+
+      if (Input.GetMouseButtonUp(0))
+      {
+        isShoot = true;
+        bucket.transform.localPosition = Vector3.zero;
+        DrawLine();
+
+      }
     }
-
-    Vector2 startPoint;
-    void Update()
+  }
+  void FixedUpdate()
+  {
+    if (isShoot)
     {
-        if (!EventSystem.current.IsPointerOverGameObject()){
-
-
-
-            if (Input.GetMouseButtonDown(0)) 
-            {
-                startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                
-            }
-
-            if (Input.GetMouseButton(0))
-            {
-                Vector2 currentPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                Vector2 dir = startPoint - currentPoint;
-                dir = Vector2.ClampMagnitude(dir, 3f);
-
-                bucket.transform.position = transform.position - new Vector3(dir.x, dir.y, 0);
-
-
-                shootDir = transform.position - bucket.transform.position;
-                DrawLine();
-          
-            }
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                isShoot = true;
-                bucket.transform.localPosition = Vector3.zero;
-                DrawLine();
-
-            }
-        }
+      ShootItem(shootDir);
+      isShoot = false;
     }
-    void FixedUpdate()
+  }
+
+  void DrawLine()
+  {
+    handleLineRenderer.SetPosition(0, linePoints[0].position);
+    handleLineRenderer.SetPosition(1, bucket.transform.position);
+    handleLineRenderer.SetPosition(2, linePoints[1].position);
+  }
+
+  public void AddItem(GameObject obj)
+  {
+    items.Add(obj);
+    obj.transform.parent = bucket.transform;
+    obj.transform.localPosition = Vector3.zero;
+  }
+
+  void ShootItem(Vector3 shootDir)
+  {
+    for (int i = 0; i < items.Count; i++)
     {
-        if (isShoot)
-        {
-            ShootingObject(shootDir);
-            isShoot = false; 
-        }
+      items[i].GetComponent<Rigidbody2D>().AddForce(shootDir * speed, ForceMode2D.Impulse);
+      items[i].transform.parent = transform.root;
     }
-
-    void DrawLine()
-    {
-        handleLineRenderer.SetPosition(0, linePoints[0].position);
-        handleLineRenderer.SetPosition(1, bucket.transform.position);
-        handleLineRenderer.SetPosition(2, linePoints[1].position);
-    }
-
-
-
-    public void AddItem(GameObject obj)
-    {
-        items.Add(obj);
-        obj.transform.parent = bucket.transform;
-        obj.transform.localPosition = Vector3.zero;
-    }
-
-    void ShootingObject(Vector3 shootDir)
-    {
-        for(int i = 0; i< items.Count; i++)
-        {
-            items[i].GetComponent<Rigidbody2D>().AddForce(shootDir * speed, ForceMode2D.Impulse);
-            items[i].transform.parent = transform.root;
-        }
-    }
+  }
 }
